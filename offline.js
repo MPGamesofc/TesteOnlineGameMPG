bc1db4fde6125f5ebbf0acb3b0a19759204f914f
MPGames <mpgamesdeveloper@gmail.com>
2019-08-21 00:53:51 -0400

    new updates!

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1566361242,
+	"version": 1566363179,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,6 +10,7 @@
 		"images/player-sheet0.png",
 		"images/player-sheet1.png",
 		"images/fireball-sheet0.png",
+		"images/paredes-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
